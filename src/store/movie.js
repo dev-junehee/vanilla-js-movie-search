e6ebcd5a93a67a215fca3d4fb32caabd00ef13c8
junehee <dev.junehee@gmail.com>
2023-05-11 20:02:49 +0900

    FEAT: 오류 출력 기능 추가

--- src/store/movie.js
+++ src/store/movie.js
@@ -17,18 +17,21 @@ export const searchMovies = async page => {
     store.state.movies = []
     store.state.message = ''
   }
-  const result = await fetch(`https://www.omdbapi.com/?apikey=7035c60c&s=${store.state.searchText}&page=${page}}`)
-  const { Search, totalResults, Response, Error } = await result.json()
-  if (Response === 'True') {
-    store.state.movies = [
-      ...store.state.movies,
-      ...Search
-    ]
-    store.state.pageMax = Math.ceil(Number(totalResults) / 10)
-  } else {
-    store.state.message = Error
+  try {
+    const result = await fetch(`https://www.omdbapi.com/?apikey=7035c60c&s=${store.state.searchText}&page=${page}}`)
+    const { Search, totalResults, Response, Error } = await result.json()
+    if (Response === 'True') {
+      store.state.movies = [
+        ...store.state.movies,
+        ...Search
+      ]
+      store.state.pageMax = Math.ceil(Number(totalResults) / 10)
+    } else {
+      store.state.message = Error
+    }
+  } catch (error) {
+    console.log('searchMovies error:', error)
+  } finally {
+    store.state.loading = false
   }
-  
-  
-  store.state.loading = false
 } 
