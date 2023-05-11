338e598776b6b18bf75281d17f6ea24653644541
junehee <dev.junehee@gmail.com>
2023-05-11 16:31:34 +0900

    FEAT: 영화 검색 기능 추가

--- src/store/movie.js
+++ src/store/movie.js
@@ -8,7 +8,14 @@ const store = new Store({
 
 export default store
 export const searchMovies = async page => {
+  if (page === 1) {
+    store.state.page = 1
+    store.state.movies = []
+  }
   const result = await fetch(`https://www.omdbapi.com/?apikey=7035c60c&s=${store.state.searchText}&page=${page}}`)
-  const json = await result.json()
-  console.log(json)
-}
+  const { Search } = await result.json()
+  store.state.movies = [
+    ...store.state.movies,
+    ...Search
+  ]
+} 
