a52cbeed8007cba557b95e944b61353c584997a7
junehee <dev.junehee@gmail.com>
2023-05-12 17:49:10 +0900

    DOCS: About 컴포넌트 추가

---
+++ src/routes/About.js
@@ -0,0 +1,10 @@
+import { Component } from "../core/core";
+
+export default class About extends Component {
+  render() {
+    this.el.classList.add('container', 'about')
+    this.el.innerHTML = /* html */ `
+      
+    `
+  }
+}
